8e1e689e2ab95ddad2ae7d96df46bb9f2db805ad
Momin Shamoon <shamunmomin17@gmail.com>
2025-11-02 15:59:16 +0530

    Updated backend with new image storage logic

--- src/main/java/com/sam/SamOutlet/configuration/WebConfig.java
+++ src/main/java/com/sam/SamOutlet/configuration/WebConfig.java
@@ -3,11 +3,18 @@ package com.sam.SamOutlet.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer{
 
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/uploads/**")
+				.addResourceLocations("file:uploads/"); // or absolute path if needed
+	}
 
 	
 	@Bean
@@ -16,7 +23,7 @@ public class WebConfig implements WebMvcConfigurer{
 	        @Override
 	        public void addCorsMappings(CorsRegistry registry) {
 	            registry.addMapping("/**")
-	                    .allowedOrigins("http://localhost:4200")
+	                    .allowedOrigins("https://shamunmomin.github.io/sam-outlet","https://shamunmomin.github.io/sam-outlet/")
 	                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 	                    .allowedHeaders("*");
 	        }
